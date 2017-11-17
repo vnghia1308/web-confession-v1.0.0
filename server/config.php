69196df02260f62d1d8fd0884f14f9db5177d66e
Vy Nghia <dora.nghia@gmail.com>
2017-11-17 20:03:39 +0700

    Update config.php

--- server/config.php
+++ server/config.php
@@ -1,12 +1,12 @@
 <?php
 /* >_ Developed by Vy Nghia */
 require 'lib/class/confession.class.php';
-define('WEBURL', 'http://support.hoitruongdep.com');
+define('WEBURL', 'http://domain.com');
 
 $db = new Database;
 $db->dbhost('localhost');
-$db->dbuser('htd_support');
-$db->dbpass('1151985611');
-$db->dbname('htd_support');
+$db->dbuser('username');
+$db->dbpass('password');
+$db->dbname('db_name');
 
 $db->connect();
